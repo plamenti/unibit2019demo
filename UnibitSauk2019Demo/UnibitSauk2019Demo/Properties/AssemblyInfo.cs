a80930fd5017127a33e17b6cf935a29133472250
plamenti <plamenti_@abv.bg>
2019-11-28 11:45:13 +0200

    Initial project with tests

---
+++ UnibitSauk2019Demo/UnibitSauk2019Demo/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnibitSauk2019Demo")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnibitSauk2019Demo")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("34e91417-2738-43f1-99fc-6dae32c53172")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ UnibitSauk2019Demo/UnibitSauk2019Demo/TestLogin.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace UnibitSauk2019Demo
+{
+    [TestClass]
+    public class TestLogin
+    {
+        private IWebDriver driver;
+        public IDictionary<string, object> vars { get; private set; }
+        private IJavaScriptExecutor js;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            driver = new ChromeDriver();
+            js = (IJavaScriptExecutor)driver;
+            vars = new Dictionary<string, object>();
+        }
+
+        [TestCleanup]
+        public void TearDown()
+        {
+            driver.Quit();
+        }
+
+        [TestMethod]
+        public void SuccessfulLogin()
+        {
+            driver.Navigate().GoToUrl("http://automationpractice.com/index.php");
+            driver.Manage().Window.Size = new System.Drawing.Size(1552, 840);
+            driver.FindElement(By.LinkText("Sign in")).Click();
+            driver.FindElement(By.Id("email")).Click();
+            driver.FindElement(By.Id("email")).SendKeys("d2041118@urhen.com");
+            driver.FindElement(By.Id("passwd")).Click();
+            driver.FindElement(By.Id("passwd")).SendKeys("123456");
+            driver.FindElement(By.CssSelector("#SubmitLogin > span")).Click();
+            Assert.AreEqual(driver.FindElement(By.CssSelector(".account > span")).Text, "Unibit Test");
+        }
+
+        [TestMethod]
+        public void EmptyFields()
+        {
+            driver.Navigate().GoToUrl("http://automationpractice.com/index.php");
+            driver.Manage().Window.Size = new System.Drawing.Size(1552, 840);
+            driver.FindElement(By.LinkText("Sign in")).Click();
+            driver.FindElement(By.CssSelector("#SubmitLogin > span")).Click();
+            Assert.AreEqual(driver.FindElement(By.CssSelector("ol > li")).Text, "An email address required.");
+        }
+        [TestMethod]
+        public void NonExistingMail()
+        {
+            driver.Navigate().GoToUrl("http://automationpractice.com/index.php");
+            driver.Manage().Window.Size = new System.Drawing.Size(1552, 840);
+            driver.FindElement(By.LinkText("Sign in")).Click();
+            driver.FindElement(By.Id("email")).Click();
+            driver.FindElement(By.Id("email")).SendKeys("non_existing_mail@test.com");
+            driver.FindElement(By.Id("passwd")).Click();
+            driver.FindElement(By.Id("passwd")).SendKeys("123456");
+            driver.FindElement(By.CssSelector("#SubmitLogin > span")).Click();
+            Assert.AreEqual(driver.FindElement(By.CssSelector("ol > li")).Text, "Authentication failed.");
+        }
+    }
+}
